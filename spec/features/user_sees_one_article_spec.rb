9b72b4f2f637743549c50f8ac6e7ca6490dadc80
Jordan Whitten <jordanwa1947@gmail.com>
2019-09-30 16:32:51 -0600

    Add article count and time since comment posted to comments section

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -17,6 +17,7 @@ describe 'User sees one article' do
       expect(page).to have_content(comment_1.body)
       expect(page).to have_content(comment_2.author_name)
       expect(page).to have_content(comment_2.body)
+      expect(page).to have_content('Comments(2)')
     end
   end
 
@@ -35,6 +36,7 @@ describe 'User sees one article' do
       expect(page).to have_content('Post a Comment')
       expect(page).to have_content('me')
       expect(page).to have_content('so awesome')
+      expect(page).to have_content('Comments(1)')
     end
   end
 end
