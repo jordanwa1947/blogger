7907fcb5aaad4d7a157c81e1cd4ece355252d120
Jordan Whitten <jordanwa1947@gmail.com>
2019-09-29 21:18:28 -0600

    Updates edit feature test

--- spec/features/user_edits_an_article_spec.rb
+++ spec/features/user_edits_an_article_spec.rb
@@ -8,11 +8,11 @@ describe 'User Edits an Article' do
       visit article_path(article)
       click_link 'Edit'
 
-      expect(current_path).to eq(edit_article)
+      expect(current_path).to eq(edit_article_path(article))
 
       fill_in 'article[title]', with: 'Title 2'
       fill_in 'article[body]', with: 'Body 2'
-      click_on 'Submit'
+      click_on 'Update Article'
 
       expect(current_path).to eq(article_path(article))
       expect(page).to_not have_content(article.title)
