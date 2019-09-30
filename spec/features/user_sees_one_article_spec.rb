de55ef7bf37cb68f359fb75f00422dd06d156df8
Jordan Whitten <jordanwa1947@gmail.com>
2019-09-30 15:00:08 -0600

    Add feature test for comment creation

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,7 +4,7 @@ describe 'User sees one article' do
   describe 'user visits /article' do
     it 'displays information for one article' do
       article = Article.create!(title: "Title 1", body: "Body 1")
-      
+
       comment_1 = article.comments.create(author_name: 'Jordan', body: 'body_1')
       comment_2 = article.comments.create(author_name: 'Tyler', body: 'body_2')
 
@@ -17,6 +17,24 @@ describe 'User sees one article' do
       expect(page).to have_content(comment_1.body)
       expect(page).to have_content(comment_2.author_name)
       expect(page).to have_content(comment_2.body)
+    end
+  end
+
+  describe 'User fills in comment form' do
+    it 'creates a comment under the article' do
+      article = Article.create!(title: "New Title", body: "New Body")
+
+      visit article_path(article)
+
+      fill_in 'comment[author_name]', with: 'me'
+      fill_in 'comment[body]', with: 'so awesome'
+
+      click_on 'Submit'
+
+      expect(current_path).to eq(article_path(article))
+      expect(page).to have_content('Post a Comment')
+      expect(page).to have_content('me')
+      expect(page).to have_content('so awesome')
     end
   end
 end
