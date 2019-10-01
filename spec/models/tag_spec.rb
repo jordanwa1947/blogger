e8e91d9ccaec6f13a3512afa24669974e842cfc2
Jordan Whitten <jordanwa1947@gmail.com>
2019-10-01 09:23:17 -0600

    Fix errors in tests and create tag and tagging models with relationships

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Tag, type: :model do
   describe 'realations' do
-    it { should have_many(:tagings) }
+    it { should have_many(:taggings) }
     it { should have_many(:articles).through(:taggings) }
   end
 end
