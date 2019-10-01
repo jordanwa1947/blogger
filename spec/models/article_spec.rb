0cd68d94157640adfc24ca3090e62bccd6a733b7
Jordan Whitten <jordanwa1947@gmail.com>
2019-10-01 09:05:42 -0600

    Adds model tests for article and tag many to many relationship

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,5 +7,7 @@ describe Article, type: :model do
   end
   describe "relations" do
     it {should have_many(:comments)}
+    it {should have_many(:taggings)}
+    it {should have_many(:tags).through(:taggings)}
   end
 end
